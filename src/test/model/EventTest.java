2d09315ff40f2d14ac626039bed7f5ce1bdeac85
Matthew Kaye <84006075+N00d13@users.noreply.github.com>
2021-11-23 22:29:12 -0800

    Added Test for Event and Event Log

--- src/test/model/EventTest.java
+++ src/test/model/EventTest.java
@@ -30,14 +30,22 @@ public class EventTest {
     }
 
     @Test
-    public void testEquals() {
+    public void testEquals() throws InterruptedException {
         Event event1 = new Event("Different event");
         Subject event2 = new Subject("Different object");
         Event event3 = event;
+        Event event4 = new Event("Null Object");
+        event4 = null;
+        Thread.sleep(1500);
+        Event event5 = new Event("Added assignment to component");
+
 
         assertFalse(event.equals(event1));
         assertFalse(event.equals(event2));
         assertTrue(event.equals(event3));
+        assertFalse(event.equals(event4));
+        assertFalse(event.equals(event5));
+
     }
 
     @Test
